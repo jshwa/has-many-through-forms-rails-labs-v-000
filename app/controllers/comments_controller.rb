be0482eaf1301a67247d2c999b0923cdb475f26f
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-16 04:45:15 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
+    comment.user = User.find(comment_params[:user_id])
     redirect_to comment.post
   end
 
