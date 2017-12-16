d2470274ca2f38bd4a4ac372632864641f0184e5
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-16 04:02:16 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,7 @@ class Comment < ActiveRecord::Base
 
   def user_attributes=(user_attributes)
     user_attributes.values.each do |user_attribute|
-      user = User.find_or_create_by(user_attribute)
+      user = User.find_or_create_by(username = user_attribute)
       self.user = user
     end
   end
